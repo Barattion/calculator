0027dd261c6cd0f99cc016504ffd91218a0aafbc
Barattion <igor.lipin1999@gmail.com>
2024-10-08 19:42:05 +0900

    Calculator

---
+++ src/ClassesAndObjects.java
@@ -0,0 +1,17 @@
+public class ClassesAndObjects {
+    public static void main (String[] args) {
+        Person person1 = new Person();
+        person1.name = "Игорь";
+        person1.age  = 25;
+        person1.sayHello();
+        person1.speak();
+        Person person2 = new Person();
+        person2.name = "Алина";
+        person2.age = 23;
+        person2.sayHello();
+        person2.speak();
+
+    }
+}
+
+
---
+++ src/Person.java
@@ -0,0 +1,14 @@
+public class Person {
+
+    String name;
+    int age;
+
+    void speak() {
+        System.out.println("Меня зовут " + name + ",мне " + age + " лет");
+
+    }
+
+    void sayHello() {
+        System.out.println("Привет!");
+    }
+}
